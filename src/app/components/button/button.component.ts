6d24bbfb54588ed9cfa6f201219517f641bcbebe
Gergo Buglyo <buglyogergo@gmail.com>
2023-11-01 22:32:30 +0100

    Created button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+  @Input() text: string = 'button';
+  @Input() color: string = '#fff';
+  @Input() bg: string = '#000';
+  @Output() btnClick = new EventEmitter();
+
+  onClick = () => {
+    this.btnClick.emit();
+  };
+}
